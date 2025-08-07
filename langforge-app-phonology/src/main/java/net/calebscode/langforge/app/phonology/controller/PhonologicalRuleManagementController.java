8a2a70ea689dba64e734025c1f9d6f631454b259
Caleb Cassady <caleb.cassady@hotmail.com>
2025-08-06 22:33:28 -0400

    Create WrappingTableCell and update PhonologicalRuleManagementController to use it.

--- langforge-app-phonology/src/main/java/net/calebscode/langforge/app/phonology/controller/PhonologicalRuleManagementController.java
+++ langforge-app-phonology/src/main/java/net/calebscode/langforge/app/phonology/controller/PhonologicalRuleManagementController.java
@@ -1,16 +1,22 @@
 package net.calebscode.langforge.app.phonology.controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.TextFieldTableCell;
 import javafx.scene.layout.VBox;
 import net.calebscode.langforge.app.phonology.model.PhonologicalRuleCollectionModel;
 import net.calebscode.langforge.app.phonology.model.PhonologicalRuleModel;
+import net.calebscode.langforge.app.ui.WrappingTableCell;
 import net.calebscode.langforge.app.util.FXMLController;
 import net.calebscode.langforge.phonology.phoneme.StandardPhonemes;
 
 public class PhonologicalRuleManagementController extends VBox implements FXMLController {
 
 	@FXML private TableView<PhonologicalRuleModel> rulesTable;
+	@FXML private TableColumn<PhonologicalRuleModel, String> ruleNameColumn;
+	@FXML private TableColumn<PhonologicalRuleModel, String> ruleSourceColumn;
+	@FXML private TableColumn<PhonologicalRuleModel, String> ruleStatusColumn;
 
 	PhonologicalRuleCollectionModel rules;
 
@@ -19,6 +25,15 @@ public class PhonologicalRuleManagementController extends VBox implements FXMLCo
 
 		load(() -> {
 			rulesTable.itemsProperty().bindBidirectional(rules.rulesProperty());
+
+			ruleNameColumn.setCellFactory(TextFieldTableCell.forTableColumn());
+			ruleNameColumn.setCellValueFactory(cellData -> cellData.getValue().nameProperty());
+
+			ruleSourceColumn.setCellFactory(TextFieldTableCell.forTableColumn());
+			ruleSourceColumn.setCellValueFactory(cellData -> cellData.getValue().sourceProperty());
+
+			ruleStatusColumn.setCellFactory(WrappingTableCell.forTableColumn());
+			ruleStatusColumn.setCellValueFactory(cellData -> cellData.getValue().compileErrorProperty());
 		});
 	}
 
