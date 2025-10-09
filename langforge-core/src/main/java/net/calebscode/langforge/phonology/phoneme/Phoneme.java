6e909518c274f848a1e6e80a4805b138e0ba85f9
Caleb Cassady <caleb.cassady@hotmail.com>
2025-10-08 23:31:50 -0400

    Add phoneme tests

--- langforge-core/src/main/java/net/calebscode/langforge/phonology/phoneme/Phoneme.java
+++ langforge-core/src/main/java/net/calebscode/langforge/phonology/phoneme/Phoneme.java
@@ -7,9 +7,21 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
 
+/// Represents a single phoneme. A phoneme is a collection of feature names, each associated with
+/// a value.
 public record Phoneme(Map<String, String> features) implements PhonemeSequence {
 
 	public Phoneme(Map<String, String> features) {
+		Objects.requireNonNull(features, "features may not be null");
+		boolean containsNullKeyOrValue = features
+				.entrySet()
+				.stream()
+				.anyMatch(e -> e.getKey() == null || e.getValue() == null);
+		
+		if (containsNullKeyOrValue) {
+			throw new IllegalArgumentException("Feature names and values must not be null.");
+		}
+		
 		this.features = Collections.unmodifiableMap(features);
 	}
 
@@ -18,11 +30,22 @@ public record Phoneme(Map<String, String> features) implements PhonemeSequence {
 	}
 
 	public boolean featureValueMatches(String featureName, String featureValue) {
-		return features.containsKey(featureValue) ? Objects.equals(featureValue, features.get(featureName)) : false;
+		return getFeatureValue(featureName).map(featureValue::equals).orElse(false);
 	}
 
 	public Optional<String> getFeatureValue(String featureName) {
 		return Optional.ofNullable(features.get(featureName));
+	}
+	
+	public Phoneme withFeature(String featureName, String featureValue) {
+		if (featureValueMatches(featureName, featureValue)) {
+			return this;
+		}
+		
+		var newFeatures = new HashMap<>(features);
+		newFeatures.put(featureName, featureValue);
+		
+		return new Phoneme(newFeatures);
 	}
 
 	public Phoneme withoutFeature(String featureName) {
